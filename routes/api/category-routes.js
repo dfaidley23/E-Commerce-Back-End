fa0c6800bcbde29f4017edca8eed21c6ce13d1c0
David faidley <david.faidley23@gmail.com>
2022-03-25 12:16:39 -0600

    adding put

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -39,18 +39,30 @@ router.post('/', async (req, res) => {
   // create a new category
   try {
     const categoryData = await Category.create(req.body);
+
     res.status(200).json(categoryData);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
 
+    if (!categoryData) {
+      res.status(404).json({message:'No Categories with this id'});
+      return;
+    }
+
+    res.json(categoryData);
   } catch (err) {
-
+    res.status(500).json(err);
   }
 });
 
