9255df02a3548e090b7692380700ed5ab553ee2e
David faidley <david.faidley23@gmail.com>
2022-03-26 09:26:25 -0600

    changed around code to populate when doing a Get

--- models/index.js
+++ models/index.js
@@ -4,27 +4,27 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE',
-});
-
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
 
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {model: ProductTag,}
+  through: ProductTag,
+  foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {model: ProductTag,}
+  through: ProductTag,
+  foreignKey: 'tag_id',
 });
-
 
 module.exports = {
   Product,
